fe682c34acacff8d6a39cf177738e1fcf21b550c
xiaoyifan <xiaoyifan@uchicago.edu>
2016-07-16 17:29:42 -0700

    init the base activity class as the base class of activities.

---
+++ app/src/main/java/com/uchicago/yifan/meditreader/BaseActivity.java
@@ -0,0 +1,36 @@
+package com.uchicago.yifan.meditreader;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by Yifan on 7/16/16.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setMessage("Loading...");
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
+    }
+
+
+}
