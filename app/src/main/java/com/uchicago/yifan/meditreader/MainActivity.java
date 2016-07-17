fc9c272b298d3f20771d1b42b727132827add394
xiaoyifan <xiaoyifan@uchicago.edu>
2016-07-16 17:53:41 -0700

    Enabled switch tabs.

--- app/src/main/java/com/uchicago/yifan/meditreader/MainActivity.java
+++ app/src/main/java/com/uchicago/yifan/meditreader/MainActivity.java
@@ -1,12 +1,52 @@
 package com.uchicago.yifan.meditreader;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
 public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FragmentPagerAdapter mPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
+
+            private final Fragment[] mFragments = new Fragment[]{
+                    new Fragment(),
+                    new Fragment(),
+            };
+
+            private final String[] mFragmentNames = new String[]{
+                    "Trending", "My Posts"
+            };
+            @Override
+            public Fragment getItem(int position) {
+                return mFragments[position];
+            }
+
+            @Override
+            public int getCount() {
+                return mFragments.length;
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mFragmentNames[position];
+            }
+        };
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mPagerAdapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
     }
 }
