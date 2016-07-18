f31dafcc8ca3d82c4535d849f9471b48c5ff6ee0
xiaoyifan <xiaoyifan@uchicago.edu>
2016-07-17 22:04:41 -0700

    Implement 2 queries for 2 fragments.

--- app/src/main/java/com/uchicago/yifan/meditreader/fragment/MyPostsFragment.java
+++ app/src/main/java/com/uchicago/yifan/meditreader/fragment/MyPostsFragment.java
@@ -1,7 +1,15 @@
 package com.uchicago.yifan.meditreader.fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
 /**
  * Created by Yifan on 7/16/16.
  */
 public class MyPostsFragment extends PostListFragment {
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        return databaseReference.child("user-posts")
+                .child(getUid());
+    }
 }
--- app/src/main/java/com/uchicago/yifan/meditreader/fragment/TrendingPostsFragment.java
+++ app/src/main/java/com/uchicago/yifan/meditreader/fragment/TrendingPostsFragment.java
@@ -1,7 +1,14 @@
 package com.uchicago.yifan.meditreader.fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
 /**
  * Created by Yifan on 7/16/16.
  */
 public class TrendingPostsFragment extends PostListFragment {
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        return databaseReference.child("posts").orderByChild("starCount").limitToFirst(50);
+    }
 }
