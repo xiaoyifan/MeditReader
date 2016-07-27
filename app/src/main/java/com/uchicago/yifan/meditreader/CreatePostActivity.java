7a577f3ad730325b6a12d3d6ecb7fb3f87fcd69a
xiaoyifan <xiaoyifan@uchicago.edu>
2016-07-27 00:13:28 -0700

    Added layout for create post.

--- app/src/main/java/com/uchicago/yifan/meditreader/CreatePostActivity.java
+++ app/src/main/java/com/uchicago/yifan/meditreader/CreatePostActivity.java
@@ -1,13 +1,107 @@
 package com.uchicago.yifan.meditreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import com.github.mr5.icarus.Icarus;
+import com.github.mr5.icarus.TextViewToolbar;
+import com.github.mr5.icarus.Toolbar;
+import com.github.mr5.icarus.button.Button;
+import com.github.mr5.icarus.button.FontScaleButton;
+import com.github.mr5.icarus.button.TextViewButton;
+import com.github.mr5.icarus.entity.Options;
+import com.github.mr5.icarus.popover.FontScalePopoverImpl;
+import com.github.mr5.icarus.popover.HtmlPopoverImpl;
+import com.github.mr5.icarus.popover.ImagePopoverImpl;
+import com.github.mr5.icarus.popover.LinkPopoverImpl;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class CreatePostActivity extends AppCompatActivity {
+
+    WebView webView;
+    protected Icarus icarus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
+
+        webView = (WebView) findViewById(R.id.editor);
+        TextViewToolbar toolbar = new TextViewToolbar();
+        Options options = new Options();
+        options.setPlaceholder("What's in your mind?");
+        //  img: ['src', 'alt', 'width', 'height', 'data-non-image']
+        // a: ['href', 'target']
+        options.addAllowedAttributes("img", Arrays.asList("data-type", "data-id", "class", "src", "alt", "width", "height", "data-non-image"));
+        options.addAllowedAttributes("iframe", Arrays.asList("data-type", "data-id", "class", "src", "width", "height"));
+        options.addAllowedAttributes("a", Arrays.asList("data-type", "data-id", "class", "href", "target", "title"));
+
+        icarus = new Icarus(toolbar, options, webView);
+        prepareToolbar(toolbar, icarus);
+        icarus.loadCSS("file:///android_asset/editor.css");
+        icarus.loadJs("file:///android_asset/test.js");
+        icarus.render();
+    }
+
+    private Toolbar prepareToolbar(TextViewToolbar toolbar, Icarus icarus) {
+        Typeface iconfont = Typeface.createFromAsset(getAssets(), "Simditor.ttf");
+        HashMap<String, Integer> generalButtons = new HashMap<>();
+        generalButtons.put(Button.NAME_BOLD, R.id.button_bold);
+        generalButtons.put(Button.NAME_OL, R.id.button_list_ol);
+        generalButtons.put(Button.NAME_BLOCKQUOTE, R.id.button_blockquote);
+        generalButtons.put(Button.NAME_HR, R.id.button_hr);
+        generalButtons.put(Button.NAME_UL, R.id.button_list_ul);
+        generalButtons.put(Button.NAME_ALIGN_LEFT, R.id.button_align_left);
+        generalButtons.put(Button.NAME_ALIGN_CENTER, R.id.button_align_center);
+        generalButtons.put(Button.NAME_ALIGN_RIGHT, R.id.button_align_right);
+        generalButtons.put(Button.NAME_ITALIC, R.id.button_italic);
+        generalButtons.put(Button.NAME_INDENT, R.id.button_indent);
+        generalButtons.put(Button.NAME_OUTDENT, R.id.button_outdent);
+        generalButtons.put(Button.NAME_CODE, R.id.button_math);
+        generalButtons.put(Button.NAME_UNDERLINE, R.id.button_underline);
+        generalButtons.put(Button.NAME_STRIKETHROUGH, R.id.button_strike_through);
+
+        for (String name : generalButtons.keySet()) {
+            TextView textView = (TextView) findViewById(generalButtons.get(name));
+            if (textView == null) {
+                continue;
+            }
+            textView.setTypeface(iconfont);
+            TextViewButton button = new TextViewButton(textView, icarus);
+            button.setName(name);
+            toolbar.addButton(button);
+        }
+        TextView linkButtonTextView = (TextView) findViewById(R.id.button_link);
+        linkButtonTextView.setTypeface(iconfont);
+        TextViewButton linkButton = new TextViewButton(linkButtonTextView, icarus);
+        linkButton.setName(Button.NAME_LINK);
+        linkButton.setPopover(new LinkPopoverImpl(linkButtonTextView, icarus));
+        toolbar.addButton(linkButton);
+
+        TextView imageButtonTextView = (TextView) findViewById(R.id.button_image);
+        imageButtonTextView.setTypeface(iconfont);
+        TextViewButton imageButton = new TextViewButton(imageButtonTextView, icarus);
+        imageButton.setName(Button.NAME_IMAGE);
+        imageButton.setPopover(new ImagePopoverImpl(imageButtonTextView, icarus));
+        toolbar.addButton(imageButton);
+
+        TextView htmlButtonTextView = (TextView) findViewById(R.id.button_html5);
+        htmlButtonTextView.setTypeface(iconfont);
+        TextViewButton htmlButton = new TextViewButton(htmlButtonTextView, icarus);
+        htmlButton.setName(Button.NAME_HTML);
+        htmlButton.setPopover(new HtmlPopoverImpl(htmlButtonTextView, icarus));
+        toolbar.addButton(htmlButton);
+
+        TextView fontScaleTextView = (TextView) findViewById(R.id.button_font_scale);
+        fontScaleTextView.setTypeface(iconfont);
+        TextViewButton fontScaleButton = new FontScaleButton(fontScaleTextView, icarus);
+        fontScaleButton.setPopover(new FontScalePopoverImpl(fontScaleTextView, icarus));
+        toolbar.addButton(fontScaleButton);
+        return toolbar;
     }
 }
