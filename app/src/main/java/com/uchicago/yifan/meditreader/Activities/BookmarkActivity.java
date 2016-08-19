ba1d724689d30e850c4dd825e163c17057d708f2
xiaoyifan <xiaoyifan@uchicago.edu>
2016-08-18 21:10:26 -0700

    Init empty bookmark activity.

---
+++ app/src/main/java/com/uchicago/yifan/meditreader/Activities/BookmarkActivity.java
@@ -0,0 +1,15 @@
+package com.uchicago.yifan.meditreader.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.uchicago.yifan.meditreader.R;
+
+public class BookmarkActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark);
+    }
+}
