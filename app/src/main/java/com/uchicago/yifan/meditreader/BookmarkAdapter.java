16365cee7ef3ef94c4e9c7f595fc8aa0876e4cba
xiaoyifan <xiaoyifan@uchicago.edu>
2016-08-30 23:14:06 -0700

    Init cursor adapter for loading bookmark data.

---
+++ app/src/main/java/com/uchicago/yifan/meditreader/BookmarkAdapter.java
@@ -0,0 +1,27 @@
+package com.uchicago.yifan.meditreader;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by Yifan on 8/30/16.
+ */
+public class BookmarkAdapter extends CursorAdapter {
+
+    public BookmarkAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
